23ada02bd6a39c196ff4a9a12396b96427f7f79a
Pawel Jurkowski <pjurkowski@amelco.co.uk>
2020-10-30 14:54:24 +0100

    Solution 1

--- src/main/java/org/example/CarService.java
+++ src/main/java/org/example/CarService.java
@@ -1,12 +1,24 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 /**
  * Given a class Car, and a array of cars please implement a method that will return data
  * structure allowing to getting all cars by any color.
  */
 public class CarService {
 
-    private Car[] cars = {
+    public static void main(String[] args) {
+        HashMap<String, List<String>> byColor = getByColor(cars);
+        for (Map.Entry<String, List<String>> stringListEntry : byColor.entrySet()) {
+            System.out.println(stringListEntry);
+        }
+    }
+
+    private static Car[] cars = {
                     new Car("AUDI","A1","black"),
                     new Car("AUDI","Q2","red"),
                     new Car("AUDI","A3","black"),
@@ -37,5 +49,25 @@ public class CarService {
         public String getColor() {
             return color;
         }
+    }
+
+    private static HashMap<String, List<String>> getByColor(Car[] cars) {
+
+        HashMap<String, List<String>> designationsMap = new HashMap<String, List<String>>();
+        List<String> carBycolor;
+
+        for (Car car : cars) {
+            if (!designationsMap.containsKey(car.getColor())) {
+                carBycolor = new ArrayList<>();
+                for (Car car2 : cars) {
+                    if (car2.getColor().equals(car.getColor())) {
+                        carBycolor.add(car2.getMake() + "-" + car2.getModel());
+                    }
+                }
+                designationsMap.put(car.getColor(), carBycolor);
+            }
+
+        }
+        return designationsMap;
     }
 }
