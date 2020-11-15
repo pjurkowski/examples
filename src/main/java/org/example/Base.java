9a89630bf4283a562aa499f030e19955bdccb576
Pawel Jurkowski <pjurkowski@amelco.co.uk>
2020-11-15 17:58:42 +0100

    Base and extension class

---
+++ src/main/java/org/example/Base.java
@@ -0,0 +1,32 @@
+package org.example;
+
+import java.util.Base64;
+
+public class Base {
+
+    static int field;
+
+    public Base(){
+        doMath(1);
+    }
+
+    void doMath(int number){
+        field += number;
+    }
+
+    static class Extension extends Base {
+
+        public Extension(){
+            doMath(2);
+        }
+
+        void doMath(int number) {
+            field += number * 2;
+        }
+    }
+
+    public static void main(String[] args) {
+        Base base = new Extension();
+        base.doMath(2);
+    }
+}
