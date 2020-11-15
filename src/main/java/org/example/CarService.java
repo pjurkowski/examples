65e98ddeaa6a09fee5d3dd8870ac7259b45259b8
Pawel Jurkowski <pjurkowski@amelco.co.uk>
2020-11-15 17:48:53 +0100

    Remove imports and implementation, add pom file

--- src/main/java/org/example/CarService.java
+++ src/main/java/org/example/CarService.java
@@ -1,21 +1,12 @@
 package org.example;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
 /**
  * Given a class Car, and a array of cars please implement a method that will return data
- * structure allowing to getting all cars by any color.
+ * structure allowing getting all cars by any color.
  */
 public class CarService {
 
     public static void main(String[] args) {
-        HashMap<String, List<String>> byColor = getByColor(cars);
-        for (Map.Entry<String, List<String>> stringListEntry : byColor.entrySet()) {
-            System.out.println(stringListEntry);
-        }
     }
 
     private static Car[] cars = {
@@ -49,25 +40,5 @@ public class CarService {
         public String getColor() {
             return color;
         }
-    }
-
-    private static HashMap<String, List<String>> getByColor(Car[] cars) {
-
-        HashMap<String, List<String>> designationsMap = new HashMap<String, List<String>>();
-        List<String> carBycolor;
-
-        for (Car car : cars) {
-            if (!designationsMap.containsKey(car.getColor())) {
-                carBycolor = new ArrayList<>();
-                for (Car car2 : cars) {
-                    if (car2.getColor().equals(car.getColor())) {
-                        carBycolor.add(car2.getMake() + "-" + car2.getModel());
-                    }
-                }
-                designationsMap.put(car.getColor(), carBycolor);
-            }
-
-        }
-        return designationsMap;
     }
 }
