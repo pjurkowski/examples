abc8018e66b08761f6793a4c090c2e8a42636f22
Pawel Jurkowski <pjurkowski@amelco.co.uk>
2021-04-20 13:27:43 +0100

    Update ListProblem description

--- src/main/java/org/example/practical/ListProblem.java
+++ src/main/java/org/example/practical/ListProblem.java
@@ -4,8 +4,8 @@ import java.util.List;
 
 /**
  * Having a List of Objects, please count actual number of objects
- * inside given list. List can contain other lists or sets with actual objects inside.
- * Do not count list/set objects.
+ * inside given list. List can contain other lists with actual objects inside.
+ * Do not count lists
  */
 public class ListProblem {
 
